95a0bafa4efeb44804d6e2a1ac4b997dd52a4bcb
wadi2a <ben.aissa.ouadie@gmail.com>
2018-06-04 08:40:49 +0200

    creation de l'interface d'acceuil avec une interface de connexion

---
+++ src/main/controleurs/LoginController.java
@@ -0,0 +1,25 @@
+package main.controleurs;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController implements Initializable {
+
+    @FXML
+    private AnchorPane parent;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    @FXML
+    private void close_app(MouseEvent event) {
+        System.exit(0);
+    }
+
+}
