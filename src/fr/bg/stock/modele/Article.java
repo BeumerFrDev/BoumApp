2f1bf607dd63ea58725588e9352bed8fd71f0c25
LyesBELKHIRI <lyessbelkhiri@gmail.com>
2018-06-11 12:55:51 +0200

    ajout

---
+++ src/fr/bg/stock/modele/Article.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.bg.stock.modele;
+
+/**
+ *
+ * @author lyess
+ */
+public class Article {
+    
+}
